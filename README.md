044930d52e486381702dd125f9c87ac5e3c265ac
ritwikchakraborty123 <ritwikchakraborty.ritwik@gmail.com>
2021-02-13 18:09:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-wa3fah
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-wa3fah)
---
+++ src/app/demo.component.ts
@@ -0,0 +1,39 @@
+import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+
+@Component({
+  selector: "demo",
+  templateUrl: "./demo.component.html",
+  styleUrls: ["./demo.component.css"]
+})
+export class newComponent {
+  year = [];
+  curr = new Date().getFullYear();
+
+  constructor() {
+    for (let i = 0; i < 21; i++) {
+      this.year.push(this.curr - i);
+    }
+  }
+
+  title = "new app";
+  contactForm = new FormGroup({
+    firstname: new FormControl('',[Validators.required,Validators.minLength(10)]),
+    lastname: new FormControl(),
+    email: new FormControl(),
+    website: new FormControl(),
+    phone: new FormControl(),
+    address: new FormGroup({
+      city: new FormControl(),
+      zip: new FormControl(),
+      state: new FormControl()
+    }),
+    dob: new FormControl()
+  });
+
+
+
+  onSubmit() {
+    console.log(this.contactForm);
+  }
+}
